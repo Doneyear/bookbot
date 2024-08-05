a540f2ef9a1f15e30e58be04b54c11c0b0a9a631
Your Name <you@example.com>
2024-08-05 17:12:02 -0500

    update main.py to give report on book

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 def main():
-    with open("books/frakenstein.txt") as f:
+    with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(file_contents)
+    number_words = get_num_words(file_contents)
+    number_letter = get_num_letters(file_contents)
+    number_letter.sort(reverse=True, key=sort_dict)
+    report = ""
+    report += f"{number_words} words found in the document\n\n"
+    for entry in number_letter:
+        report += f"The '{entry['char']} character was found {entry['num']} times \n"
+    print(report)
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_num_letters(text):
+    letters = {}
+    dict_list = []
+    for l in text:
+        if l.isalpha():
+            lowered = l.lower()
+            if lowered in letters:
+                letters[lowered] += 1
+            else: letters[lowered] = 1
+    for letter, count in letters.items():
+        entry = {'char': letter, 'num': count}
+        dict_list.append(entry)
+    return dict_list
+
+def sort_dict(dict):
+    return dict["num"]
 
 if __name__ == "__main__":
     main()
